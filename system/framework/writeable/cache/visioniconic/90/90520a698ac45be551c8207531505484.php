0df323c4f21931f9c2aed7a8863470131b98fd61
Elwira Perdana <flymecosupply@gmail.com>
2025-01-31 14:19:02 +0800

    add framework directory

---
+++ system/framework/writeable/cache/visioniconic/90/90520a698ac45be551c8207531505484.php
@@ -0,0 +1,66 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* indonesia/contact.rapid.php */
+class __TwigTemplate_557e9a180d7ab4f7fa2037e079f1cdab extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 1
+        yield "<h1>Contact</h1>";
+        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "indonesia/contact.rapid.php";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  42 => 1,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("", "indonesia/contact.rapid.php", "C:\\xampp\\htdocs\\visionframework-3\\visioniconic-v5\\app\\visioniconic\\views\\indonesia\\contact.rapid.php");
+    }
+}
